05f655531e84db026429cbd5acd1b1c62a4211d4
UmarkhonAkhmedov <umarkhonakhmedov2000@gmail.com>
2021-09-23 15:57:28 +0500

    Doing menu project

--- Questions/app.js
+++ Questions/app.js
@@ -1,5 +1,5 @@
 // Using selectors inside the element
-
+ 
 const questions = document.querySelectorAll(".question");
 questions.forEach(function(question){
   const btn = question.querySelector(".question-btn");
