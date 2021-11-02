ec37d0d28a88b40dbcd0306f8f74031261beb031
UmarkhonAkhmedov <76143018+UmarkhonAkhmedov@users.noreply.github.com>
2021-11-02 17:47:10 +0500

    Doing grocery bud project with removing all data, and adding html and few parts of javascript

--- Grocery/app.js
+++ Grocery/app.js
@@ -1,22 +1,23 @@
-// ****** SELECT ITEMS **********
+// Selecte Items
 const alert = document.querySelector('.alert');
 const form = document.querySelector('.grocery-form');
 const grocery = document.getElementById('grocery');
 const submitBtn = document.querySelector('.submit-btn');
 const container = document.querySelector('.grocery-container');
-const list = document.querySelector('grocery-list');
+const list = document.querySelector('.grocery-list');
 const clearBtn = document.querySelector('.clear-btn');
 
-// Edit option
-let editElement;
-let editFlag = false;
-let editId = "";
 
-// ****** EVENT LISTENERS **********
-// Submit form
+// Edit Option
+let editElement, editFlag = false;
+let editID = "";
 
-// ****** FUNCTIONS **********
+// Event Listener
+// Sumbit form
+form.addEventListener('submit', addItem);
 
-// ****** LOCAL STORAGE **********
-
-// ****** SETUP ITEMS **********
+// Functions
+function addItem(e){
+  e.preventDefault();
+  
+}
